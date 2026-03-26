9eb30d241e673c34ab36d729506dec584d43f366
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-03-26 14:50:05 +0530

    Fixed api url

--- src/api.js
+++ src/api.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
-//const API_URL = "https://totes-backend-app-service-epddbyg7hmagfvc4.centralindia-01.azurewebsites.net";
-const API_URL = "https://totes-backend-app-service-epddbyg7hmagfvc4.canadacentral-01.azurewebsites.net"; // Uncomment this if you want to use the 'canadacentral' URL instead
-
+// Vite looks for variables starting with VITE_
+// In Azure App Service (Frontend), add VITE_API_URL in Environment Variables
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
 });
 
-export const getTodos = () => api.get('/todos').then(res => res.data);
-export const createTodo = (todo) => api.post('/todos', todo).then(res => res.data);
-export const updateTodo = (id, todo) => api.put(`/todos/${id}`, todo).then(res => res.data);
-export const deleteTodo = (id) => api.delete(`/todos/${id}`).then(res => res.data);
+// Helper for cleaning responses
+const responseBody = (res) => res.data;
 
-// Notes API
-export const getNotes = () => api.get('/notes').then(res => res.data);
-export const createNote = (note) => api.post('/notes', note).then(res => res.data);
-export const updateNote = (id, note) => api.put(`/notes/${id}`, note).then(res => res.data);
-export const deleteNote = (id) => api.delete(`/notes/${id}`).then(res => res.data);
+export const getTodos = () => api.get('/todos').then(responseBody);
+export const createTodo = (todo) => api.post('/todos', todo).then(responseBody);
+export const updateTodo = (id, todo) => api.put(`/todos/${id}`, todo).then(responseBody);
+export const deleteTodo = (id) => api.delete(`/todos/${id}`).then(responseBody);
+
+export const getNotes = () => api.get('/notes').then(responseBody);
+export const createNote = (note) => api.post('/notes', note).then(responseBody);
+export const updateNote = (id, note) => api.put(`/notes/${id}`, note).then(responseBody);
+export const deleteNote = (id) => api.delete(`/notes/${id}`).then(responseBody);
